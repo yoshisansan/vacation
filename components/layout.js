73b24cb3d0b01ec074ab78f655d429e3517fb01a
AKIFUMI <awakewalker007@example.com>
2021-07-25 22:03:52 +0700

    deploy準備

--- components/layout.js
+++ components/layout.js
@@ -17,9 +17,7 @@ export default function Layout({ children }) {
       />
       <meta
         property="og:image"
-        content={`https://og-image.vercel.app/${encodeURI(
-          siteTitle
-        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        content={`/gatya-vacation-og.png`}
       />
       <meta name="og:title" content={siteTitle} />
       <meta name="twitter:card" content="summary_large_image" />
